1d9aea963bdd5b750ea403cd9dad89630592e94b
AlexShcheglov89 <alex89shcheglov@gmail.com>
2025-09-29 12:15:06 +0200

    Enhance ListingScreen with background image and styling

--- app/screens/ListingScreen/ListingScreen.tsx
+++ app/screens/ListingScreen/ListingScreen.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, ImageBackground, StyleSheet} from 'react-native';
 
 export const ListingScreen = () => {
   return (
-    <View>
-      <Text>Listing Screen</Text>
-    </View>
+    <ImageBackground
+      source={require('../../../assets/images/rickandmorty.jpg')}
+      style={styles.backgroundImage}
+      resizeMode="cover">
+      <View style={styles.container}>
+        <Text style={styles.title}>Listing Screen</Text>
+      </View>
+    </ImageBackground>
   );
 };
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  container: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
